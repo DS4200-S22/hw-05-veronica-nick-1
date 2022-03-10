a8fe1c94fc70d87b0bc26ca70f597b1f1a0eb26b
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2022-03-09 20:06:42 -0500

    Update brushinglinking.js

--- js/brushinglinking.js
+++ js/brushinglinking.js
@@ -200,7 +200,7 @@ d3.csv("data/iris.csv").then((data) => {
         // Call when Scatterplot1 is brushed 
         function updateChart1(brushEvent) {
             extent = brushEvent.selection;
-            myCircles1.classed("selected", function(d){ return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length) ) } );
+            myCircles1.classed("circle-selected", function(d){ return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length) ) } );
             myCircles2.classed("selected", function(d){ return isBrushed(extent, x1(d.Sepal_Length), y1(d.Petal_Length) ) } );
         }
 
